e0741ca44737ca1de21a3ba8cd3d2819f4d46016
josh.nice <joshliamnice@gmail.com>
2020-07-20 18:02:07 +0100

    added attribute binding

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'socks'
+        product: 'Socks',
+        image: '../assets/socks.jpg',
+        buy_socks: 'https://www2.hm.com/en_gb/productpage.0301227002.html?gclid=CjwKCAjwgdX4BRB_EiwAg8O8HfFwVxwhwEHdzclElexnDmNLrGPNaanQFQkXJHgctVwangub7cLDxhoCAcoQAvD_BwE'
     }
 })
