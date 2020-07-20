8c4017f9a46686998e39b95398394e92fb029be4
josh.nice <joshliamnice@gmail.com>
2020-07-20 20:44:19 +0100

    added if template conditions

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,8 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: '../assets/socks.jpg',
-        buy_socks: 'https://www2.hm.com/en_gb/productpage.0301227002.html?gclid=CjwKCAjwgdX4BRB_EiwAg8O8HfFwVxwhwEHdzclElexnDmNLrGPNaanQFQkXJHgctVwangub7cLDxhoCAcoQAvD_BwE'
+        buy_socks: 'https://www2.hm.com/en_gb/productpage.0301227002.html?gclid=CjwKCAjwgdX4BRB_EiwAg8O8HfFwVxwhwEHdzclElexnDmNLrGPNaanQFQkXJHgctVwangub7cLDxhoCAcoQAvD_BwE',
+        stock: 8,
+        on_sale: true
     }
 })
